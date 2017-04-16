58f045cf80937ab313d902e94ae3182d3f184923
Rob Walton <rob.walton@diamond.ac.uk>
2017-04-16 23:28:54 +0100

    Add tree view command

--- ulysses_client/ulysses.py
+++ ulysses_client/ulysses.py
@@ -494,3 +494,16 @@ def filter_items(items, title, type_='sheet_or_group'):
                 (type_ == 'sheet_or_group' or item.type == type_)):
             filtered_items.append(item)
     return filtered_items
+
+
+def treeview(group, indent=0):
+    """"Recursively group structure returning a list of printable lines."""
+    lines = []
+    lines.append(
+        group.identifier + ' - ' + '   ' * indent + group.title + ':')
+    for sheet in group.sheets:
+        lines.append(
+            sheet.identifier + ' - ' + '   ' * (indent + 1) + sheet.title)
+    for sub_group in group.containers:
+        lines.extend(treeview(sub_group, indent + 1))
+    return lines
